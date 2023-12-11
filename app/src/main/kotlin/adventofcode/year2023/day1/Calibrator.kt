2692d7e89c6787ca2a50a27e77fade7fcd0e1821
Jaco van der Byl <jaco.vanderbyl@gmail.com>
2023-12-11 16:46:52 +0000

    Minor improvement to Day 1 Puzzle 2.

--- app/src/main/kotlin/adventofcode/year2023/day1/Calibrator.kt
+++ app/src/main/kotlin/adventofcode/year2023/day1/Calibrator.kt
@@ -15,15 +15,15 @@ class Calibrator(documentFileName: String = "year2023/day1/calibration_document"
 
     fun sum()  = calibrations.map {
         mapOf(
-            "one" to "one1one",
-            "two" to "two2two",
-            "three" to "three3three",
-            "four" to "four4four",
-            "five" to "five5five",
-            "six" to "six6six",
-            "seven" to "seven7seven",
-            "eight" to "eight8eight",
-            "nine" to "nine9nine",
+            "one" to "o1e",
+            "two" to "t2o",
+            "three" to "t3e",
+            "four" to "f4r",
+            "five" to "f5e",
+            "six" to "s6x",
+            "seven" to "s7n",
+            "eight" to "e8t",
+            "nine" to "n9e",
         ).entries.fold(it) {string, (spelledNumber, numericString) -> string.replace(spelledNumber, numericString) }
     }.map { line ->
         line.filter { it.digitToIntOrNull() != null }

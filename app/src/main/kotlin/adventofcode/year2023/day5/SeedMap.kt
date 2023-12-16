14bd545f016f0cf2511aa633fc37f6d7181a6819
Jaco van der Byl <jaco.vanderbyl@gmail.com>
2023-12-16 11:01:40 +0000

    Remove unnecessary code.

--- app/src/main/kotlin/adventofcode/year2023/day5/SeedMap.kt
+++ app/src/main/kotlin/adventofcode/year2023/day5/SeedMap.kt
@@ -4,24 +4,11 @@ class SeedMap(fileName: String = "year2023/day5/seed_maps") {
     private val seedMapsRaw = ClassLoader.getSystemResourceAsStream(fileName)?.bufferedReader()?.readText()
         .toString().replace("\r\n".toRegex(), "\n").replace("  ", " ")
 
-    private val inputRegexes = listOf(
-        """seed-to-soil map:\n(?<map>(\d+.+|\n)+)""".toRegex(),
-        """soil-to-fertilizer map:\n(?<map>(\d+.+|\n)+)""".toRegex(),
-        """fertilizer-to-water map:\n(?<map>(\d+.+|\n)+)""".toRegex(),
-        """water-to-light map:\n(?<map>(\d+.+|\n)+)""".toRegex(),
-        """light-to-temperature map:\n(?<map>(\d+.+|\n)+)""".toRegex(),
-        """temperature-to-humidity map:\n(?<map>(\d+.+|\n)+)""".toRegex(),
-        """humidity-to-location map:\n(?<map>(\d+.+|\n)+)""".toRegex(),
-    )
+    private val maps = listOf("soil", "fertilizer", "water", "light", "temperature", "humidity", "location")
 
     fun lowestLocation() : Long {
         val seeds = getSeedsFromInput()
-        val initRangeMaps = mutableListOf<Map<LongRange,LongRange>>()
-        val rangeMaps = mutableListOf<Map<LongRange,LongRange>>()
-
-        inputRegexes.mapTo(initRangeMaps) { regex -> createRangeMap(getMapsFromInput(regex)) }
-        initRangeMaps.mapTo(rangeMaps) { rangeMap -> fillRangeMap(rangeMap, maxIndexOf(initRangeMaps)) }
-
+        val rangeMaps = maps.map{ createRangeMap(getMapData(it)) }
         return seeds.minOf { getDestination(rangeMaps, it) }
     }
 
@@ -30,8 +17,8 @@ class SeedMap(fileName: String = "year2023/day5/seed_maps") {
             it.toLong()
         } ?: listOf()
 
-    private fun getMapsFromInput(regex: Regex) : List<Triple<Long,Long,Long>> = regex.find(seedMapsRaw)
-        ?.groups?.get("map")?.value?.split("\n".toRegex())?.filter { it.isNotEmpty() }
+    private fun getMapData(name: String) : List<Triple<Long,Long,Long>> = """$name map:\n(?<map>(\d+.+|\n)+)"""
+        .toRegex().find(seedMapsRaw)?.groups?.get("map")?.value?.split("\n".toRegex())?.filter { it.isNotEmpty() }
         ?.map {
             val mapValues = it.split(" ")
             Triple(mapValues[0].toLong(), mapValues[1].toLong(), mapValues[2].toLong())
@@ -45,24 +32,6 @@ class SeedMap(fileName: String = "year2023/day5/seed_maps") {
         return rangeMap.toSortedMap(compareBy { it.first }).toMap()
     }
 
-    private fun maxIndexOf(rangeMaps: List<Map<LongRange,LongRange>>) : Long = rangeMaps.map { map ->
-        map.keys.map {it.last } }.maxOf { it.max() }
-
-    private fun fillRangeMap(rangeMap: Map<LongRange,LongRange>, maxIndex: Long) : Map<LongRange,LongRange> {
-        val newRangeMap = mutableMapOf<LongRange,LongRange>()
-        var first = 0L
-        var last = 0L
-
-        rangeMap.keys.toList().sortedBy { it.first }.forEach {
-            if (first < it.first) newRangeMap[first..<it.first] = first..<it.first
-            first = it.last + 1
-            last = it.last
-        }
-        if (last < maxIndex) newRangeMap[last + 1..maxIndex] = last + 1..maxIndex
-
-        return (rangeMap.toMutableMap() + newRangeMap).toSortedMap(compareBy { it.first }).toMap()
-    }
-
     private fun getDestination(rangeMap: Map<LongRange,LongRange>, key: Long) : Long {
         var value: Long? = null
         rangeMap.forEach { (sourceRange, destinationRange) ->
@@ -72,7 +41,7 @@ class SeedMap(fileName: String = "year2023/day5/seed_maps") {
                 return@forEach
             }
         }
-        return value ?: throw IndexOutOfBoundsException()
+        return value ?: key
     }
 
     private fun getDestination(rangeMaps: List<Map<LongRange,LongRange>>, seed: Long) : Long {

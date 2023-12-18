a597c97ec74cbe890b899d979760579a02648aa9
Jaco van der Byl <jaco.vanderbyl@gmail.com>
2023-12-18 16:10:27 +0000

    Rename val.

--- src/main/kotlin/year2023/Day01Puzzle01.kt
+++ src/main/kotlin/year2023/Day01Puzzle01.kt
@@ -4,9 +4,9 @@ package year2023
  * https://adventofcode.com/2023/day/1
  */
 class Day01Puzzle01(fileName: String = "year2023/input_day01") {
-    private val inputFile = ClassLoader.getSystemResourceAsStream(fileName)?.bufferedReader()
+    private val inputReader = ClassLoader.getSystemResourceAsStream(fileName)?.bufferedReader()
 
-    fun run() = generateSequence { inputFile?.readLine() }.map { line ->
+    fun run() = generateSequence { inputReader?.readLine() }.map { line ->
         line.filter { char -> char.isDigit() }.let {
             if (it.isNotEmpty()) "${it.first()}${it.last()}".toInt() else 0
         }

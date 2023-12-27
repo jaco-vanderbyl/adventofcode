8dee4038ccbb35fcc9f72bedddde6f83534b5799
Jaco van der Byl <jaco.vanderbyl@gmail.com>
2023-12-27 15:28:16 +0000

    Minor refactor.

--- src/main/kotlin/year2023/Day05Puzzle02.kt
+++ src/main/kotlin/year2023/Day05Puzzle02.kt
@@ -8,16 +8,26 @@ import kotlin.math.abs
 class Day05Puzzle02(fileName: String = "year2023/input_day05") {
     private val inputReader = ClassLoader.getSystemResourceAsStream(fileName)?.bufferedReader()
 
-    enum class MapTypes { SOIL, FERTILIZER, WATER, LIGHT, TEMPERATURE, HUMIDITY, LOCATION }
+    // Consider all the initial seed numbers listed in the ranges on the first line of the almanac.
+    // What is the lowest location number that corresponds to any of the initial seed numbers?
+    fun run(): Long {
+        val inputStr = inputReader?.readText()?.replace("\r\n", "\n")?.replace("  ", " ") ?: ""
+        val seeds = Seeds(ranges = getSeedRanges(inputStr))
+        val maps = Maps(ranges = MapType.entries.map { mapType -> getMapRanges(inputStr, mapType) }.flatten())
+        return maps.lowestLocation(seeds) ?: throw IllegalArgumentException()
+    }
+
+    enum class MapType { SOIL, FERTILIZER, WATER, LIGHT, TEMPERATURE, HUMIDITY, LOCATION }
 
     data class SeedRange(val start: Long, val rangeSize: Long) {
         val range = start..<(start + rangeSize)
     }
 
-    data class MapRange(val mapType: MapTypes, val destinationStart: Long, val sourceStart: Long, val rangeSize: Long) {
-        val sourceRange = sourceStart..<(sourceStart + rangeSize)
-        val destinationRange = destinationStart..<(destinationStart + rangeSize)
-        fun max() : Long = maxOf(sourceRange.last, destinationRange.last)
+    data class MapRange(val mapType: MapType, val destinationStart: Long, val sourceStart: Long, val rangeSize: Long) {
+        val source = sourceStart..<(sourceStart + rangeSize)
+        val destination = destinationStart..<(destinationStart + rangeSize)
+        val first = minOf(source.first, destination.first)
+        val last = maxOf(source.last, destination.last)
     }
 
     class Seeds(private val ranges: List<SeedRange>) {
@@ -30,14 +40,14 @@ class Day05Puzzle02(fileName: String = "year2023/input_day05") {
     }
 
     class Maps(private val ranges: List<MapRange>) {
-        private val locationRange = 0..ranges.maxOf { it.max() }
+        private val fullRange = ranges.minOf { it.first }..ranges.maxOf { it.last }
 
         /**
          * Using the smallest seed range size as the step size,
          * step over the location range until a location that maps to a legit seed is found,
          * then recursively adjust the range and step-size, until the lowest location is found.
          */
-        fun lowestLocation(seeds: Seeds, range: LongRange = locationRange, step: Long = seeds.minRangeSize) : Long? {
+        fun lowestLocation(seeds: Seeds, range: LongRange = fullRange, step: Long = seeds.minRangeSize) : Long? {
             // As range sizes are reduced recursively, ensure that range can be stepped through at least twice.
             val halfRangeDiff = abs((range.last - range.first + 2) / 2)
             val newStep = if (step <= halfRangeDiff) step else halfRangeDiff
@@ -45,12 +55,12 @@ class Day05Puzzle02(fileName: String = "year2023/input_day05") {
             var lastLocation = range.first
             for (location in range step newStep) {
                 if (seeds.inRange(findSeed(location))) {
-                    // Range size has been reduced to 1, the seed with the lowest location has been found.
+                    // The range size has been reduced to 1; the seed with the lowest location has been found.
                     if (range.last - range.first == 0L) return location
 
                     // A location that maps to a legit seed has been found in the range.
                     // The lowest location must be somewhere in the last step; set range accordingly for the next pass.
-                    // The next pass will start to reduce the step sizes, and this condition will not be true again.
+                    // The next pass will start to reduce the step sizes, and this condition will never be true again.
                     if (step <= halfRangeDiff) return lowestLocation(seeds, (location - newStep)..location, newStep)
 
                     // The lowest location is either in the first or second half of the range.
@@ -73,27 +83,18 @@ class Day05Puzzle02(fileName: String = "year2023/input_day05") {
 
         private fun findSeed(location: Long) : Long {
             var value = location
-            MapTypes.entries.reversed().forEach { mapType -> value = lookupSource(mapType, value) }
+            MapType.entries.reversed().forEach { mapType -> value = lookupSource(mapType, value) }
             return value
         }
 
-        private fun lookupSource(mapType: MapTypes, destination: Long) : Long {
+        private fun lookupSource(mapType: MapType, destination: Long) : Long {
             ranges.filter { mapRange -> mapRange.mapType == mapType }.forEach { mapRange ->
-                if (destination in mapRange.destinationRange) {
-                    return mapRange.sourceRange.first + (destination - mapRange.destinationRange.first)
+                if (destination in mapRange.destination) {
+                    return mapRange.source.first + (destination - mapRange.destination.first)
                 }
             }
             return destination
         }
-    }
-
-    // Consider all the initial seed numbers listed in the ranges on the first line of the almanac.
-    // What is the lowest location number that corresponds to any of the initial seed numbers?
-    fun run(): Long {
-        val inputStr = inputReader?.readText()?.replace("\r\n", "\n")?.replace("  ", " ") ?: ""
-        val seeds = Seeds(ranges = getSeedRanges(inputStr))
-        val maps = Maps(ranges = MapTypes.entries.map { mapType -> getMapRanges(inputStr, mapType) }.flatten())
-        return maps.lowestLocation(seeds) ?: throw IllegalArgumentException()
     }
 
     private fun getSeedRanges(str: String): List<SeedRange> = """\d+ \d+""".toRegex().findAll(
@@ -103,7 +104,7 @@ class Day05Puzzle02(fileName: String = "year2023/input_day05") {
         SeedRange(seedValues[0].toLong(), seedValues[1].toLong())
     }.toList()
 
-    private fun getMapRanges(str: String, mapType: MapTypes) : List<MapRange> {
+    private fun getMapRanges(str: String, mapType: MapType) : List<MapRange> {
         return """${mapType.name.lowercase()} map:\n(?<map>(\d+.+|\n)+)""".toRegex().find(
             str
         )?.groups?.get("map")?.value?.split("\n".toRegex())?.filter { it.isNotEmpty() }?.map {
